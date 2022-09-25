4f5e241de211b2923cd44b4f4616fbe1da0ff03f
merryoncherry <cherry@merryoncherry.org>
2022-09-25 13:31:08 -0400

    Unlikely to be useful to anyone else: my deploymnt

---
+++ Deploy.py
@@ -0,0 +1,34 @@
+import subprocess
+
+srcnet="c:\\users\\chuck\\documents\\xlightsShows\\2022_Base\\xlights_networks.xml"
+intnet="c:\\users\\chuck\\documents\\xlightsShows\\2022_Halloween\\xlights_networks.xml"
+dstnet="\\\\desktop-kara3a2\\users\\chuck\\documents\\xlightsShows\\2022_Halloween\\xlights_networks.xml"
+
+srcrgb="c:\\users\\chuck\\documents\\xlightsShows\\2022_Base\\xlights_rgbeffects.xml"
+intrgb="c:\\users\\chuck\\documents\\xlightsShows\\2022_Halloween\\xlights_rgbeffects.xml"
+dstrgb="\\\\desktop-kara3a2\\users\\chuck\\documents\\xlightsShows\\2022_Halloween\\xlights_rgbeffects.xml"
+
+subprocess.run([
+    "python",
+    "LayoutUtils/pyLayout.py", 
+    "--layout="+srcrgb,
+    "--outlayout="+intrgb,
+    "--edit=InGroup=OnlyForXmas:Delete:true;InGroup=OnlyForFuture:Delete:true;Type=Obj:Brighten:30;Obj=.*_Xmas:Active:false;Model=.*:dimcurveall:0,2.5;Model=TreeFence:dimcurvergb:-14,2.5,0,2.5,0,2.5;Model=MainMatrix:dimcurveall:-60,2.6;Model=Arch Hedge.*:dimcurveall:-60,2.5",
+    "--transform=translate:0,0,-300;roty:30"
+    ], shell=True, check=True)
+
+subprocess.run([
+    "copy", "/Y",
+    srcnet,
+    intnet], shell=True, check=True)
+
+subprocess.run([
+    "copy", "/Y",
+    intnet,
+    dstnet], shell=True, check=True)
+
+subprocess.run([
+    "copy","/Y",
+    intrgb,
+    dstrgb], shell=True, check=True)
+
