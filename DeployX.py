4d8e7e93fd180bd0725dd1dcc2eb822753211eaa
merryoncherry <cherry@merryoncherry.org>
2022-11-13 12:31:21 -0500

    Scripts for xmas

---
+++ DeployX.py
@@ -0,0 +1,35 @@
+import subprocess
+import xlAutomation.xlDo
+
+srcnet="c:\\users\\chuck\\documents\\xlightsShows\\2022_Base\\xlights_networks.xml"
+intnet="c:\\users\\chuck\\documents\\xlightsShows\\2022_Xmas\\xlights_networks.xml"
+dstnet="\\\\desktop-kara3a2\\users\\chuck\\documents\\xlightsShows\\2022_Xmas\\xlights_networks.xml"
+
+srcrgb="c:\\users\\chuck\\documents\\xlightsShows\\2022_Base\\xlights_rgbeffects.xml"
+intrgb="c:\\users\\chuck\\documents\\xlightsShows\\2022_Xmas\\xlights_rgbeffects.xml"
+dstrgb="\\\\desktop-kara3a2\\users\\chuck\\documents\\xlightsShows\\2022_Xmas\\xlights_rgbeffects.xml"
+
+subprocess.run([
+    "python",
+    "LayoutUtils/pyLayout.py", 
+    "--layout="+srcrgb,
+    "--outlayout="+intrgb,
+    "--edit=InGroup=OnlyForHalloween:Delete:true;InGroup=OnlyForFuture:Delete:true;Type=Obj:Brighten:30;Obj=.*_Halloween:Active:false;Model=.*:dimcurveall:0,2.3;Model=TreeFence:dimcurvergb:-14,2.5,0,2.5,0,2.5;Model=MainMatrix:dimcurveall:-60,2.2;Model=Arch Hedge.*:dimcurveall:-60,2.3;Model=PPD GE Baby Grand.*:dimcurveall:-30,2.2",
+    "--transform=translate:0,0,-300;roty:30"
+    ], shell=True, check=True)
+
+subprocess.run([
+    "copy", "/Y",
+    srcnet,
+    intnet], shell=True, check=True)
+
+subprocess.run([
+    "copy", "/Y",
+    intnet,
+    dstnet], shell=True, check=True)
+
+subprocess.run([
+    "copy","/Y",
+    intrgb,
+    dstrgb], shell=True, check=True)
+
---
+++ RerenderX.py
@@ -0,0 +1,49 @@
+import subprocess
+import xlAutomation.xlDo
+
+srcnet="c:\\users\\chuck\\documents\\xlightsShows\\2022_Base\\xlights_networks.xml"
+intnet="c:\\users\\chuck\\documents\\xlightsShows\\2022_Xmas\\xlights_networks.xml"
+dstnet="\\\\desktop-kara3a2\\users\\chuck\\documents\\xlightsShows\\2022_Xmas\\xlights_networks.xml"
+
+srcrgb="c:\\users\\chuck\\documents\\xlightsShows\\2022_Base\\xlights_rgbeffects.xml"
+intrgb="c:\\users\\chuck\\documents\\xlightsShows\\2022_Xmas\\xlights_rgbeffects.xml"
+dstrgb="\\\\desktop-kara3a2\\users\\chuck\\documents\\xlightsShows\\2022_Xmas\\xlights_rgbeffects.xml"
+
+xlenv = xlAutomation.xlDo.XLEnv()
+xlenv.data = 'c:\\Users\\Chuck\\Documents\\xlightsShows\\2022_Xmas'
+
+wasRunning = xlenv.isXLightsRunning(True)
+
+if not wasRunning:
+    xlenv.startXLights()
+
+xlenv.changeShowFolder()
+
+if False:
+    msg = xlenv.batchRenderSeqList([
+    "GodBlessTheUSA.xsq",
+    "MyHero.xsq",
+    "ROCKInTheUSA.xsq",
+    "PartyInTheUSA.xsq",
+    "VetsBG.xsq",
+    ])
+    print (msg)
+
+if False:
+    msg = xlenv.batchRenderSeqList([
+    ])
+    print (msg)
+
+if False:
+    msg = xlenv.batchRenderSeqList([
+    ])
+    print (msg)
+
+if not wasRunning:
+   xlenv.stopXLights()
+
+subprocess.run([
+    "copy", "/Y",
+    "c:\\users\\chuck\\documents\\xlightsShows\\2022_Xmas\\*.fseq",
+    "\\\\desktop-kara3a2\\users\\chuck\\documents\\xlightsShows\\2022_Xmas"], shell=True, check=True)
+
