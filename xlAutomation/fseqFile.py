255cd506aae22cc7f0838d9e56ca00fd7b50b7a5
merryoncherry <cherry@merryoncherry.org>
2023-01-23 11:42:43 -0500

    And now we read the models, give or take end chan

--- xlAutomation/fseqFile.py
+++ xlAutomation/fseqFile.py
@@ -38,6 +38,15 @@ def allzero(d):
             return False
     return True
 
+class ModelRec:
+    def __init__(self, name, startch, nch):
+        self.name = name
+        self.startch = startch
+        self.nch = nch
+
+    def __repr__(self):
+        return self.name + ":" + str(self.startch) + "," + str(self.nch)
+
 if __name__ == '__main__':
     # Command line arguments
     parser = argparse.ArgumentParser()
@@ -49,7 +58,7 @@ if __name__ == '__main__':
 
     controllers = []
     ctrlbyname = {}
-    models = None
+    models = []
     if args.xlights:
         xmodels   = xml.dom.minidom.parse(args.xlights+'/xlights_rgbeffects.xml')
         xnetworks = xml.dom.minidom.parse(args.xlights+'/xlights_networks.xml')
@@ -73,6 +82,42 @@ if __name__ == '__main__':
                 if (net.tagName != 'network'):
                     continue
                 startch += int(net.getAttribute('MaxChannels'))
+
+        xnd = xmodels.documentElement
+        if (xnd.tagName != 'xrgb'):
+            raise Exception('Root not "xrgb"')
+        for grp in xnd.childNodes:
+            if grp.nodeType == xml.dom.Node.ATTRIBUTE_NODE or grp.nodeType == xml.dom.Node.TEXT_NODE:
+                continue
+            if grp.tagName != 'models':
+                continue
+            for mdl in grp.childNodes:
+                if mdl.nodeType == xml.dom.Node.ATTRIBUTE_NODE or mdl.nodeType == xml.dom.Node.TEXT_NODE:
+                    continue
+                if (mdl.tagName != 'model'):
+                    continue
+                name = mdl.getAttribute('name')
+                chstr = mdl.getAttribute('StartChannel')
+                channel = -1
+                if (chstr[0] >= '0' and chstr[0] <= '9') :
+                    channel = int(chstr)
+                elif chstr[0] == '@':
+                    continue
+                elif chstr[0] == '!':
+                    # TODO Look up controller
+                    (ctrlnm,offset) = chstr[1:].split(':')
+                    channel = ctrlbyname[ctrlnm]+int(offset)-1
+                else:
+                    raise Exception("Unknown channel string: "+chstr)
+                models.append(ModelRec(name, channel, -1))
+
+        # Oh heck how to calculate channels per model
+        #  Will we eventually just have to add specific logic?
+        smodels = sorted(models, key = lambda m : m.startch)
+        for i in range(0, len(smodels)):
+            if i == len(smodels)-1 :
+                continue
+            smodels[i].nch = smodels[i+1].startch - smodels[i].startch
 
     hjson = {}
     with open(args.flist[0], 'rb') as fh:
@@ -238,3 +283,4 @@ if __name__ == '__main__':
 
         print(json.dumps(hjson, indent=2))
         print(str(controllers))
+        print(str(smodels))
