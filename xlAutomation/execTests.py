d45a8a18eed3ccc407604dcc28aa025004b7673d
merryoncherry <cherry@merryoncherry.org>
2023-01-26 07:17:51 -0500

    Does the things

--- xlAutomation/execTests.py
+++ xlAutomation/execTests.py
@@ -2,7 +2,7 @@ import subprocess
 
 
 #subprocess.run([self.bin+"\\"+"xlDo.exe", "-c", cmd], capture_output=False)
-if 0:
+if 1:
     subprocess.run('python fseqFile.py '
         " -s m:\\users\\chuck\\source\\repos\\merryoncherry\\xlts\\showfolders\\effectsonstars\\EffectsOnStars.xsq "
         " -x m:\\users\\chuck\\source\\repos\\merryoncherry\\xlts\\showfolders\\effectsonstars "
