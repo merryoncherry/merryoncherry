905c51c39216748c9f8a7c02b65828d4b86fbb7e
merryoncherry <cherry@merryoncherry.org>
2023-01-28 17:34:51 -0500

    Keep working on sheet generator

--- perf2xlsx.py
+++ perf2xlsx.py
@@ -1,12 +1,16 @@
 import argparse
+import json
 import xlsxwriter
 
 # Read the JSON of each perf report
 # Order is determined by first file of occurence for item
 
+# python perf2xlsx -o RenderPerfComp.xlsx M:\xL_Test_2021\2021_Aspirational_Perf_2021_39\perf_report.json  M:\xL_Test_2021\2021_Aspirational_Perf_2022_13\perf_report.json  M:\xL_Test_2021\2021_Aspirational_Perf_2022_26\perf_report.json 
+
 # Command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-o', '--output', help="Path to output worksheed")  
+parser.add_argument('-o', '--output', help="Path to output worksheet")  
+parser.add_argument('files', nargs='*', help="Input files")
 
 args = parser.parse_args()
 
@@ -14,30 +18,55 @@ ofn = args.output
 if not ofn:
     ofn = 'xlPerf.xlsx'
 
+jsons = []
+for fn in args.files:
+    with open(fn, 'r') as fh:
+        jsons.append(json.load(fh))
+
+# Summary
+# start_xlights_start
+# start_xlights_end
+# stop_xlightx_start
+# stop_xlights_end
+
+# Per Suite
+# switch folder start end
+
+# Per test
+# cmp_start, cmp_end
+# crc_start, crc_end
+# start_batch, end_batch
+# start_/end_ open/render/save/close
+
 # Create a workbook and add a worksheet.
 workbook = xlsxwriter.Workbook(ofn)
+
+# Add a bold format to use to highlight cells.
+bold = workbook.add_format({'bold': True})
+
+# Add a number format for cells with money.
+# money = workbook.add_format({'num_format': '$#,###'})
+
 worksheet = workbook.add_worksheet('Summary')
-
-# Some data we want to write to the worksheet.
-expenses = (
-    ['Rent', 1000],
-    ['Gas',   100],
-    ['Food',  300],
-    ['Gym',    50],
-)
-
-# Start from the first cell. Rows and columns are zero indexed.
-row = 0
-col = 0
-
-# Iterate over the data and write it out row by row.
-for item, cost in (expenses):
-    worksheet.write(row, col,     item)
-    worksheet.write(row, col + 1, cost)
-    row += 1
-
-# Write a total using a formula.
-worksheet.write(row, 0, 'Total')
-worksheet.write(row, 1, '=SUM(B1:B4)')
+# Header
+worksheet.write(0, 0, 'Event', bold)
+c = 1
+r = {}
+nr = 1
+for j in jsons:
+    worksheet.write(0, c, args.files[c-1], bold)
+    for k in j.keys():
+        if k.startswith('start_'):
+            bk = k[6:]
+            ek = 'end_' + bk
+            if bk in r:
+                row = r[bk]
+            else:
+                row = nr
+                nr = nr + 1
+                r[bk] = row
+                worksheet.write(row, 0, bk)
+            worksheet.write(row, c, j[ek]-j[k])
+    c = c + 1
 
 workbook.close()
