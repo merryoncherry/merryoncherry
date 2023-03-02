4553356a87cf6cbe45ddcd75d463f580b0d48c4b
merryoncherry <cherry@merryoncherry.org>
2023-03-02 09:48:52 -0500

    In upstream

--- MoCbuild/mocbuild.bat
+++ MoCbuild/mocbuild.bat
@@ -7,9 +7,6 @@ git apply ..\merryoncherry\MoCbuild\ctrl.diff
 rem Disable dark mode
 git apply ..\merryoncherry\MoCbuild\dark.diff
 
-rem Ripple rotation value curve not working
-git apply ..\merryoncherry\MoCbuild\ripple.diff
-
 rem Don't warn about old FPP
 git apply ..\merryoncherry\MoCbuild\nowarn.diff
 
