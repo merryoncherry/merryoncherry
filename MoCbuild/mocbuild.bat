21f76983ed26915360e5f135116749f9da88624f
merryoncherry <cherry@merryoncherry.org>
2023-03-30 22:29:39 -0400

    Diff for ripply

--- MoCbuild/mocbuild.bat
+++ MoCbuild/mocbuild.bat
@@ -7,11 +7,11 @@ git apply ..\merryoncherry\MoCbuild\ctrl.diff
 rem Don't warn about old FPP
 git apply ..\merryoncherry\MoCbuild\nowarn.diff
 
-rem Submodel tools
-git apply ..\merryoncherry\MoCbuild\submodeltools.diff
-
 rem Label size in grids
 git apply ..\merryoncherry\MoCbuild\labelsize.diff
+
+rem Ripple effect extensions
+git apply ,,\merryoncherry\MoCbuild\ripple.diff
 
 rem Set version to moc
 git apply ..\merryoncherry\MoCbuild\version.diff
