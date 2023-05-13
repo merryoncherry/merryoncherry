68335e98dbd0069af98278e967c2d07ea3b2c843
merryoncherry <cherry@merryoncherry.org>
2023-05-13 07:48:02 -0400

    Some patch cleanup

--- MoCbuild/mocbuild.bat
+++ MoCbuild/mocbuild.bat
@@ -1,9 +1,6 @@
 rem Arches don't that zigzag
 git apply ..\merryoncherry\MoCbuild\arch_zigzag.diff
 
-rem Issue when starting without .xcontroller files
-git apply ..\merryoncherry\MoCbuild\ctrl.diff
-
 rem Don't warn about old FPP
 git apply ..\merryoncherry\MoCbuild\nowarn.diff
 
