b33f31c46b3fa3ad2b2c90f28725e22795f621f4
Chuck B <chuck.bear@dbos.dev>
2024-07-14 14:41:22 -0400

    For Linux

---
+++ MoCbuild/mocbuild.sh
@@ -0,0 +1,12 @@
+:
+# Label size in grids
+git apply ../merryoncherry/MoCbuild/labelsize.diff
+
+# Set version to moc
+git apply ../merryoncherry/MoCbuild/version.diff
+
+# Add descriptions
+git apply ../merryoncherry/MoCbuild/descriptions.diff
+
+# Preview vid CLI
+git apply ../merryoncherry/MoCbuild/previewvid.diff
