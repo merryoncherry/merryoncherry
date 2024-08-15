36c795d637e501f61bec8dd9d9f6d208de394617
unknown <cherry@merryoncherry.org>
2024-08-14 22:58:40 -0400

    Up level for 2024

--- MoCbuild/mocbuild.sh
+++ MoCbuild/mocbuild.sh
@@ -1,12 +1,16 @@
 :
 # Label size in grids
+echo "Label size in grids"
 git apply ../merryoncherry/MoCbuild/labelsize.diff
 
 # Set version to moc
+echo "Set Version"
 git apply ../merryoncherry/MoCbuild/version.diff
 
 # Add descriptions
+echo "Descriptions"
 git apply ../merryoncherry/MoCbuild/descriptions.diff
 
 # Preview vid CLI
+echo "Preview vid"
 git apply ../merryoncherry/MoCbuild/previewvid.diff
--- MyShow/DeployH.py
+++ MyShow/DeployH.py
@@ -1,20 +1,20 @@
 import subprocess
 import xlAutomation.xlDo
 
-srcnet="c:\\users\\chuck\\documents\\xlightsShows\\2023_Base\\xlights_networks.xml"
-intnet="c:\\users\\chuck\\documents\\xlightsShows\\2023_Halloween\\xlights_networks.xml"
-dstnet="\\\\192.168.1.133\\users\\chuck\\documents\\xlightsShows\\2023_Halloween\\xlights_networks.xml"
+srcnet="c:\\users\\chuck\\documents\\xlightsShows\\2024_Base\\xlights_networks.xml"
+intnet="c:\\users\\chuck\\documents\\xlightsShows\\2024_Halloween\\xlights_networks.xml"
+dstnet="\\\\192.168.1.133\\users\\chuck\\documents\\xlightsShows\\2024_Halloween\\xlights_networks.xml"
 
-srcrgb="c:\\users\\chuck\\documents\\xlightsShows\\2023_Base\\xlights_rgbeffects.xml"
-intrgb="c:\\users\\chuck\\documents\\xlightsShows\\2023_Halloween\\xlights_rgbeffects.xml"
-dstrgb="\\\\192.168.1.133\\users\\chuck\\documents\\xlightsShows\\2023_Halloween\\xlights_rgbeffects.xml"
+srcrgb="c:\\users\\chuck\\documents\\xlightsShows\\2024_Base\\xlights_rgbeffects.xml"
+intrgb="c:\\users\\chuck\\documents\\xlightsShows\\2024_Halloween\\xlights_rgbeffects.xml"
+dstrgb="\\\\192.168.1.133\\users\\chuck\\documents\\xlightsShows\\2024_Halloween\\xlights_rgbeffects.xml"
 
 subprocess.run([
     "python",
     "LayoutUtils/pyLayout.py", 
     "--layout="+srcrgb,
     "--outlayout="+intrgb,
-    "--edit=InGroup=OnlyForXmas:Delete:true;InGroup=OnlyForFuture:Delete:true;Type=Obj:Brighten:30;Obj=.*_Xmas:Active:false;Model=.*:dimcurveall:0,2.2;Model=TreeFence:dimcurvergb:-14,2.2,0,2.2,0,2.2;Model=MainMatrix:dimcurveall:-60,2.2;Model=Arch Hedge.*:dimcurveall:-60,2.2;Model=PPD GE Baby Grand.*:dimcurveall:-30,2.2;Model=GE Dragonfly.*:dimcurveall:0,2.5;Model=GE Franken Monster.*:dimcurveall:0,2.5;Model=GE Priem Cube.*:dimcurveall:0,2.5;Model=Matrix KBR Window.*:dimcurveall:0,2.5;Model=MatrixFWall.*:dimcurveall:0,2.5;Model=MatrixPost.*:dimcurveall:0,2.5;Model=MatrixSideDoor.*:dimcurveall:0,2.5;Model=PPD GE Baby Grand Illusion.*:dimcurveall:0,2.5;Model=StarF.*:dimcurveall:0,2.5;Model=GE Triune Tomb.*:dimcurveall:0,2.5;Model=GE Insane.*:dimcurveall:0,1.0;Model=DmxWands:dimcurveall:0,2.2;Model=DmxWandsCtrl:dimcurveall:0,1.0;Model=HohohoBushBase:dimcurveall:-60,2.2",
+    "--edit=InGroup=OnlyForXmas:Delete:true;InGroup=OnlyForFuture:Delete:true;Type=Obj:Brighten:30;Obj=.*_Xmas:Active:false;Model=.*:dimcurveall:0,2.2;Model=TreeFence:dimcurvergb:-14,2.2,0,2.2,0,2.2;Model=MainMatrix:dimcurveall:-60,2.2;Model=Arch Hedge.*:dimcurveall:-60,2.2;Model=GE Dragonfly.*:dimcurveall:0,2.5;Model=GE Dazzler.*:dimcurveall:0,2.5;Model=GE Star Gazer.*:dimcurveall:0,2.5;Model=GE Franken Monster.*:dimcurveall:0,2.5;Model=GE Priem Cube.*:dimcurveall:0,2.5;Model=Matrix KBR Window.*:dimcurveall:0,2.5;Model=MatrixFWall.*:dimcurveall:0,2.5;Model=MatrixPost.*:dimcurveall:0,2.5;Model=MatrixSideDoor.*:dimcurveall:0,2.5;Model=PPD GE Baby Grand Illusion.*:dimcurveall:0,2.5;Model=StarF.*:dimcurveall:0,2.5;Model=GE Triune Tomb.*:dimcurveall:0,2.5;Model=GE Insane.*:dimcurveall:0,1.0;Model=DmxWands:dimcurveall:0,2.2;Model=DmxWandsCtrl:dimcurveall:0,1.0;Model=HohohoBushBase:dimcurveall:-60,2.2",
     "--transform=translate:0,0,-300;roty:30"
     ], shell=True, check=True)
 
